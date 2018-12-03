f35d769d3ceea873da5258ff5dbcf166428c5ba2
Gomez Emmanuel <manu_capo_3@hotmail.com>
2018-12-03 14:55:36 -0300

    Agregar archivos de proyecto.

---
+++ AgendaDistribuidas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AgendaDistribuidas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
